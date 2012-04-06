87ba6c01340c376e869340bc271684473cae4f37
Tim Schmiedl <timschmiedl@gmx.net>
2012-04-06 21:50:50 +0200

    First Commit from Eclipse

--- src/de/timweb/android/activity/MainActivity.java
+++ src/de/timweb/android/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
