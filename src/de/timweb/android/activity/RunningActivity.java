ae1164df82d3e0d5054c114419f56fb00e4f33a9
Tim Schmiedl <timschmiedl@gmx.net>
2012-06-06 22:27:13 +0200

    organize Imports

--- src/de/timweb/android/activity/RunningActivity.java
+++ src/de/timweb/android/activity/RunningActivity.java
@@ -8,7 +8,6 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
