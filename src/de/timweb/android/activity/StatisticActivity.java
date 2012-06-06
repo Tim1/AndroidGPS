f1ea46ee3fe9e5bf95e9bbf4def1fa78c12c9e8b
Tim Schmiedl <timschmiedl@gmx.net>
2012-06-06 23:10:11 +0200

    new Icons for TabView

--- src/de/timweb/android/activity/StatisticActivity.java
+++ src/de/timweb/android/activity/StatisticActivity.java
@@ -1,15 +1,12 @@
 package de.timweb.android.activity;
 
-import de.timweb.android.track.Track;
-import de.timweb.android.track.TrackManager;
-import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.TabHost;
-import android.widget.Toast;
 
+@SuppressWarnings("deprecation")
 public class StatisticActivity extends TabActivity{
 
 	private int trackId;
@@ -30,7 +27,7 @@ public class StatisticActivity extends TabActivity{
 	    intent = new Intent().setClass(this, DiagrammActivity.class);
 	    intent.putExtra("mode", 1);
 	    intent.putExtra("_id", trackId);
-	    spec = tabHost.newTabSpec("diagram_1").setIndicator("Diagramm 1",
+	    spec = tabHost.newTabSpec("diagram_1").setIndicator("Graph 1",
 	                      res.getDrawable(R.drawable.ic_tab_diagramm_1))
 	                  .setContent(intent);
 	    tabHost.addTab(spec);
@@ -40,7 +37,7 @@ public class StatisticActivity extends TabActivity{
 	    intent = new Intent().setClass(this, DiagrammActivity.class);
 	    intent.putExtra("mode", 2);
 	    intent.putExtra("_id", trackId);
-	    spec = tabHost.newTabSpec("diagramm_2").setIndicator("Diagramm 2",
+	    spec = tabHost.newTabSpec("diagramm_2").setIndicator("Graph 2",
 	                      res.getDrawable(R.drawable.ic_tab_diagramm_2))
 	                  .setContent(intent);
 	    tabHost.addTab(spec);
@@ -48,12 +45,16 @@ public class StatisticActivity extends TabActivity{
 	  
 	    intent = new Intent().setClass(this, GoogleMapActivity.class);
 	    intent.putExtra("_id", trackId);
-	    spec = tabHost.newTabSpec("googlemap").setIndicator("GoogleMap",
+	    spec = tabHost.newTabSpec("googlemap").setIndicator("Map",
 	    		res.getDrawable(R.drawable.ic_tab_googlemap))
 	    		.setContent(intent);
 	    tabHost.addTab(spec);
 	    
 //	    tabHost.setCurrentTab(2);
+	    /**
+	     * Alles in einem Tab wuerde ich Sagen (dann sinds 4 Tabs)
+	     * @author Tim
+	     */
 	   //noch zwei für notes und bewertung oder alles in einer
 		
 	}
